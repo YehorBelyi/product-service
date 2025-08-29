c3797f02f42572787b0b206b21a8e314797a693d
Yehor Belyi <egorwhite06@gmail.com>
2025-08-29 19:12:58 +0300

    Some fixes

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import CreateCheckoutSessionView, OrderSuccessView, OrderCancelView, OrderCreateView, stripe_webhook_view, \
-    OrderConfirmationView
+    OrderConfirmationView, OrderConfirmCancelView
 
 urlpatterns = [
     path('create-checkout-session/<int:order_id>', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('cancel/', OrderCancelView.as_view(), name='order_cancel'),
     path('confirm/<int:order_id>', OrderConfirmationView.as_view(), name='confirm-order'),
     path('webhooks/stripe/', stripe_webhook_view, name='stripe-webhook'),
+    path('cancel/<int:order_id>', OrderConfirmCancelView.as_view(), name='order-confirm-cancel'),
 ]
